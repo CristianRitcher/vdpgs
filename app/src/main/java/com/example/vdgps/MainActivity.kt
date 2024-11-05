26d35a041bf587b703455481295694e268a4c322
Cristian Ritcher <cristian.ritcher@outlook.com>
2024-11-04 20:54:42 -0600

    addition of bottom navbar

--- app/src/main/java/com/example/vdgps/MainActivity.kt
+++ app/src/main/java/com/example/vdgps/MainActivity.kt
@@ -5,25 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        // Load the MapFragment
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, MapFragment()) // Ensure fragment_container exists in your activity layout
-                .commit()
-        }
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
+        // Set initial fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MapFragment())
+            .commit()
+
+        bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
+            val selectedFragment = when (menuItem.itemId) {
+                R.id.home -> MapFragment()
+                R.id.search -> MapFragment()
+                R.id.profile -> MapFragment()
+                else -> null
+            }
+            selectedFragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, it)
+                    .commit()
+            }
+            true
+        }
     }
-
 }
