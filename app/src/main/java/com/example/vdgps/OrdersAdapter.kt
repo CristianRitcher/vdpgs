a149fca9f6355b17f72a24bee9db6dcf1856ebc5
Cristian Ritcher <cristian.ritcher@outlook.com>
2024-12-02 23:19:15 -0600

    grammar

--- app/src/main/java/com/example/vdgps/OrdersAdapter.kt
+++ app/src/main/java/com/example/vdgps/OrdersAdapter.kt
@@ -28,19 +28,19 @@ class OrdersAdapter(
 
     // Configurar los datos para cada ítem del RecyclerView
     override fun onBindViewHolder(holder: OrdersViewHolder, position: Int) {
-        val order = ordersList[position];
+        val order = ordersList[position]
 
         // Mostrar información de depuración en Logcat
-        Log.d("OrdersAdapter", "Order at position $position: ${order.titulo}, ${order.descripcion}");
+        Log.d("OrdersAdapter", "Order at position $position: ${order.titulo}, ${order.descripcion}")
 
         // Configura los datos del elemento
-        holder.tituloTextView.text = order.titulo;
-        holder.descripcionTextView.text = order.descripcion;
+        holder.tituloTextView.text = order.titulo
+        holder.descripcionTextView.text = order.descripcion
 
         // Configura el clic del botón de edición
-        holder.editButton.setOnClickListener { onEditClick(order) };
+        holder.editButton.setOnClickListener { onEditClick(order) }
     }
 
     // Devolver el número de ítems en la lista
-    override fun getItemCount(): Int = ordersList.size;
+    override fun getItemCount(): Int = ordersList.size
 }
